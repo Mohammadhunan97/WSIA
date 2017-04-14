d90c37fce914519805e0a70ef1d8da00a5e4bd9c
Christopher Costello <christophercostello97@gmail.com>
2017-04-14 14:33:44 -0400

    Add files via upload

--- WSIA website/general.js
+++ WSIA website/general.js
@@ -1,12 +1,30 @@
-// Position left and right border around active navbar item
+// Set content minimum height (DISABLED)
+// function setContentMinimumHeight() {
+// 	var contentHeight = $(document).height() - ($('#header').height() + $('#footer').height());
+// 	$('#content').css('min-height', contentHeight - 2); // until fix border-boxing
+// }
+// Exclude scrollbars from width (NOT WORKING)
+function excludeScrollbarsFromWidth() {
+	// Check if body height is higher than window height
+	if ($("body").height() > $(window).height()) {
+		$('body').css('padding-bottom', '15px');
+	} else {
+		$('body').css('padding-bottom', '0');
+	}
+	// Check if body width is higher than window width
+	if ($('body').width() > $(window).width()) {
+		$('body').css('padding-right', '15px');
+	} else {
+		$('body').css('padding-right', '0');
+	}
+}
+// Position left and right borders around active navbar item
 function positionActiveBorder() {
-  if ($('li.active').contains('ul.subnavbar') == true) {
-    $('li.active').addClass('before-active after-active');
-  }
-  else {
-    $('li.active').prev('li').addClass('before-active');
-    $('li.active').next('li').addClass('after-active');
-  }
+	$("#active").each(function () {
+		$(this).has("ul").addClass('active-drop');		
+		$(this).not(':has(ul)').prev('li').addClass('before-active');
+		$(this).not(':has(ul)').next('li').addClass('after-active');
+	});
 }
 // Position subnavbar function
 function positionSubnavbar() {
@@ -20,83 +38,87 @@ function positionContent() {
 	// Calculate height of navbar
 	var navHeight = $("#navbar").height();
 	// Make top margin of content equal to navbar height
-	$("#content").css('margin-top', navHeight + 'px');
+	$("#content").css('margin-top', navHeight + 2 + 'px'); // + 2px until I implement border-box in navbar
 }
 // Main function
 $(document).ready(function() {
-  // Position left and right border around active navbar item
-  // positionActiveBorder()
+	// Exclude scrollbars from width
+	excludeScrollbarsFromWidth()
+	// Position left and right borders around active navbar item
+	positionActiveBorder()
 	// Position subnavbar
 	positionSubnavbar();
 	// Position content
 	positionContent();
-	// On resize
+	// On resize...
 	$(window).resize(function() {
+		// Exclude scrollbars from width
+		excludeScrollbarsFromWidth()
 		// Reposition content
 		positionContent();
-  });
+	});
 });
-/* Anchor fix (by Ian Clark) */
+/* Fixed header anchor fix (by Ian Clark) */
 (function(document, history, location) {
-  var HISTORY_SUPPORT = !!(history && history.pushState);
-  var anchorScrolls = {
-    ANCHOR_REGEX: /^#[^ ]+$/,
-    OFFSET_HEIGHT_PX: 70,
-    /**
-     * Establish events, and fix initial scroll position if a hash is provided.
-     */
-    init: function() {
-      this.scrollToCurrent();
-      $(window).on('hashchange', $.proxy(this, 'scrollToCurrent'));
-      $('body').on('click', 'a', $.proxy(this, 'delegateAnchors'));
-    },
-    /**
-     * Return the offset amount to deduct from the normal scroll position.
-     * Modify as appropriate to allow for dynamic calculations
-     */
-    getFixedOffset: function() {
-      return this.OFFSET_HEIGHT_PX;
-    },
-    /**
-     * If the provided href is an anchor which resolves to an element on the
-     * page, scroll to it.
-     * @param  {String} href
-     * @return {Boolean} - Was the href an anchor.
-     */
-    scrollIfAnchor: function(href, pushToHistory) {
-      var match, anchorOffset;
-      if(!this.ANCHOR_REGEX.test(href)) {
-        return false;
-      }
-      match = document.getElementById(href.slice(1));
-      if(match) {
-        anchorOffset = $(match).offset().top - this.getFixedOffset();
-        $('html, body').animate({ scrollTop: anchorOffset});
-        // Add the state to history as-per normal anchor links
-        if(HISTORY_SUPPORT && pushToHistory) {
-          history.pushState({}, document.title, location.pathname + href);
-        }
-      }
-      return !!match;
-    },
-    /**
-     * Attempt to scroll to the current location's hash.
-     */
-    scrollToCurrent: function(e) { 
-      if(this.scrollIfAnchor(window.location.hash) && e) {
-      	e.preventDefault();
-      }
-    },
-    /**
-     * If the click event's target was an anchor, fix the scroll position.
-     */
-    delegateAnchors: function(e) {
-      var elem = e.target;
+	var HISTORY_SUPPORT = !!(history && history.pushState);
+	var anchorScrolls = {
+		ANCHOR_REGEX: /^#[^ ]+$/,
+		OFFSET_HEIGHT_PX: 70,
+		/**
+		 * Establish events, and fix initial scroll position if a hash is provided.
+		 */
+		init: function() {
+			this.scrollToCurrent();
+			$(window).on('hashchange', $.proxy(this, 'scrollToCurrent'));
+			$('body').on('click', 'a', $.proxy(this, 'delegateAnchors'));
+		},
+		/**
+		 * Return the offset amount to deduct from the normal scroll position.
+		 * Modify as appropriate to allow for dynamic calculations
+		 */
+		getFixedOffset: function() {
+			return this.OFFSET_HEIGHT_PX;
+		},
+		/**
+		 * If the provided href is an anchor which resolves to an element on the
+		 * page, scroll to it.
+		 * @param  {String} href
+		 * @return {Boolean} - Was the href an anchor.
+		 */
+		scrollIfAnchor: function(href, pushToHistory) {
+			var match, anchorOffset;
+			if(!this.ANCHOR_REGEX.test(href)) {
+				return false;
+			}
+			match = document.getElementById(href.slice(1));
+			if(match) {
+				anchorOffset = $(match).offset().top - this.getFixedOffset();
+				$('html, body').animate({ scrollTop: anchorOffset});
+				// Add the state to history as-per normal anchor links
+				if(HISTORY_SUPPORT && pushToHistory) {
+					history.pushState({}, document.title, location.pathname + href);
+				}
+			}
+			return !!match;
+		},
+		/**
+		 * Attempt to scroll to the current location's hash.
+		 */
+		scrollToCurrent: function(e) { 
+			if(this.scrollIfAnchor(window.location.hash) && e) {
+				e.preventDefault();
+			}
+		},
+		/**
+		 * If the click event's target was an anchor, fix the scroll position.
+		 */
+		delegateAnchors: function(e) {
+			var elem = e.target;
 
-      if(this.scrollIfAnchor(elem.getAttribute('href'), true)) {
-        e.preventDefault();
-      }
-    }
-  };
+			if(this.scrollIfAnchor(elem.getAttribute('href'), true)) {
+				e.preventDefault();
+			}
+		}
+	};
 	$(document).ready($.proxy(anchorScrolls, 'init'));
 })(window.document, window.history, window.location);
